4b7e9682201533621688fdd4f5ea0274fde4e5f6
VladislavOvsyannikov <vlad23.1996@mail.ru>
2016-10-13 22:49:36 +0300

    Initial commit

---
+++ src/MyCode.java
@@ -0,0 +1,14 @@
+public class MyCode implements Runnable{
+
+    static int t=1;
+
+    public void run() {
+        for (int i=1;i<=10000;i++){
+            System.out.println(getFree());
+        }
+    }
+
+    public synchronized int getFree(){
+            return t++;
+    }
+}
